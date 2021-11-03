ef74fb7838450fc1e86077f9cfe1ff83899cab04
Jeferson Goncalves <jeferson.goncalves.noronha@gmail.com>
2021-11-03 10:37:35 -0300

    add radioButton e atualização em nome dos botões

--- Mobile/ComponenteBasicos/app/src/main/java/com/example/componentebasicos/MainActivity2.kt
+++ Mobile/ComponenteBasicos/app/src/main/java/com/example/componentebasicos/MainActivity2.kt
@@ -4,8 +4,11 @@ import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebView
 import android.widget.MediaController
+import android.widget.RadioGroup
+import android.widget.Toast
 import android.widget.VideoView
 import androidx.annotation.RequiresApi
 import android.media.session.MediaController as MediaController1
@@ -17,6 +20,7 @@ class MainActivity2 : AppCompatActivity() {
         setContentView(R.layout.activity_main2)
 
         web()
+
         
     }
 
@@ -24,6 +28,17 @@ class MainActivity2 : AppCompatActivity() {
         val myWebView: WebView = findViewById(R.id.webview)
         myWebView.loadUrl("https://www.google.com.br/")
         myWebView.settings.javaScriptEnabled = true
+    }
+
+    fun sexo(view: View){
+        val sexo = findViewById<RadioGroup>(R.id.sexo)
+
+        if(sexo.checkedRadioButtonId == R.id.radioButton){
+            Toast.makeText(applicationContext, "você selecionou o sexo Masculino", Toast.LENGTH_LONG).show()
+        }else if(sexo.checkedRadioButtonId == R.id.radioButton2){
+            Toast.makeText(applicationContext, "você selecionou o sexo Femenino", Toast.LENGTH_LONG).show()
+        }
+
     }
 
 }
